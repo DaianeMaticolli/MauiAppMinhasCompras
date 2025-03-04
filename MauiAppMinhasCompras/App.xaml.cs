f3cf8baf45534e826afdaa3701ce7021a3aee7b7
DaianeMaticolli <daiane_maticolli@hotmail.com>
2025-03-04 17:09:00 -0300

    Add Models e troca de tela inicial

--- MauiAppMinhasCompras/App.xaml.cs
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -1,12 +1,27 @@
-﻿namespace MauiAppMinhasCompras
+﻿using MauiAppMinhasCompras.Helpers;
+
+namespace MauiAppMinhasCompras
 {
     public partial class App : Application
     {
+        static SQLiteDatabaseHelper _db;
+
+        public static SQLiteDatabaseHelper Db
+        {
+            get
+            {
+                if(_db == null)
+                {
+                    _db = new SQLiteDatabaseHelper("....... db3");
+                }
+                return _db;
+            }
+                    }
         public App()
         {
             InitializeComponent();
-
             MainPage = new AppShell();
+            MainPage = new NavigationPage(new Views.ListarProduto());
         }
     }
 }
