bce80e06e43667465fc36e1797d558bc17de7362
DaianeMaticolli <daiane_maticolli@hotmail.com>
2025-03-18 18:58:58 -0300

    Validação da inserção de produtos

--- MauiAppMinhasCompras/Models/Produto.cs
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -4,25 +4,52 @@ namespace MauiAppMinhasCompras.Models
 {
     public class Produto
     {
-        string _descricao;
+        private string _descricao;
+        private double _quantidade;
+        private double _preco;
 
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
+
         public string Descricao
         {
             get => _descricao;
             set
             {
-                if (value == null)
+                if (string.IsNullOrWhiteSpace(value))
                 {
-                    throw new Exception("Por favor, preencha a descrição");
+                    throw new ArgumentException("Por favor, preencha a descrição!");
                 }
-
-                _descricao = value;
+                _descricao = value.Trim();
             }
         }
-        public double Quantidade { get; set; }
-        public double Preco { get; set; }
-        public double Total { get => Quantidade * Preco; }
+
+        public double Quantidade
+        {
+            get => _quantidade;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("A quantidade deve ser maior que zero!");
+                }
+                _quantidade = value;
+            }
+        }
+
+        public double Preco
+        {
+            get => _preco;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("O preço deve ser maior que zero!");
+                }
+                _preco = value;
+            }
+        }
+
+        public double Total => Quantidade * Preco;
     }
 }
